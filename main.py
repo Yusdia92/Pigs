959266b22f10a57d1ecdbb5aef2c0087f9005b32
Yusdia92 <93882893+Yusdia92@users.noreply.github.com>
2024-10-07 22:45:48 +0700

    Pigs.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import os
 
 
 async def main():
-    print("Soft's author: https://t.me/ApeCryptor\n")
+    print("Soft's author: https://t.me/dailychipercombo\n")
     action = int(input("Select action:\n0. About soft\n1. Start soft\n2. Get statistics\n3. Create sessions\n\n> "))
 
     if action == 0:
